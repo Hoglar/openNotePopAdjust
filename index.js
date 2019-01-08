ad53f24fc6e810fd69d1071eb4ce73a69fe018b3
Hoglar <martinhl__@hotmail.com>
2019-01-08 12:19:09 +0100

    its Done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const MongoClient = require('mongodb').MongoClient;
 
 // So what is this?
 
@@ -9,11 +9,45 @@
 
 // Infinite recursion?
 
+const dbName = "notes";
+const url = 'mongodb://localhost:27017';
+
 function adjustPopularityEveryDay(i) {
     setTimeout(() => {
         console.log('Infinite Loop has been going for how many days?', i);
+
+        (async function() {
+  // Connection URL
+          const client = new MongoClient(url, { useNewUrlParser: true });
+
+          try {
+            // Use connect method to connect to the Server
+            await client.connect();
+            console.log("Success conecting to database");
+
+            const db = client.db(dbName);
+
+            let collections = await db.listCollections().toArray();
+
+            collections.forEach((collection) => {
+                db.collection(collection.name).updateMany(
+                    {"score.popularity": { $gt: 1 }},
+                    {
+                        $mul: {
+                        "score.popularity": 0.5
+                        }
+                    }
+                )
+            })
+
+          } catch (err) {
+            console.log(err.stack);
+          }
+
+          client.close();
+})();
         adjustPopularityEveryDay(++i);
-    }, 2000)
+    }, 86400000)
 }
 
 adjustPopularityEveryDay(0);
